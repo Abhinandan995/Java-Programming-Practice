b58013b09c450b2af6a01f9d06929d0aac89e6d0
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-06-13 21:45:52 +0530

    Create Introduction1.java

---
+++ Introduction1.java
@@ -0,0 +1,8 @@
+class Introduction1
+{
+  static public void main(String args[])
+  {
+      System.out.print("Hello User");
+      System.out.println("Welcome to the new environment called 'JAVA'");
+   }
+}
