47d25765debab7a287496599119d944070f53af5
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-06-14 17:21:04 +0530

    Create Introduction3.java

---
+++ Introduction3.java
@@ -0,0 +1,13 @@
+//Introduction using Data types
+class Introduction
+{
+  public static void main(String args[])
+  {
+    String i = "Abhinandan Reddy";
+    String j = "15P31A04G0";
+    String k = "Aditya College of Engineering and Technology";
+    double l = 65.62;
+    String b = "Electronics and Communication Engineering";
+    System.out.println(i+" bearing rollnumber "+j+" just completed his Btech on "+b+" branch with a percentage of "+l+" in "+k);
+  }
+ }
