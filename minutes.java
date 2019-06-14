22c91ce73b4a84f1af179a5bc4c23cb6efc6e6ff
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-06-14 21:03:47 +0530

    Create minutes.java

---
+++ minutes.java
@@ -0,0 +1,11 @@
+//Number of minutes in a day
+class minutes
+{
+  public static void main(String args[])
+  {
+    int a = 24;  //no. of hours in a day.
+    int b = 60;  //no. of minutes in an hour.
+    long c = a*b;
+    System.out.println("Number of Minutes in a Day: "+c);
+  }
+}
