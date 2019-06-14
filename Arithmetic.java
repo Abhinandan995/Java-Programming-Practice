b98d2ec98c86652649be4315e98c2278faea50c0
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-06-14 17:47:03 +0530

    Create Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,16 @@
+//Arithmetic operators
+class Arithmetic
+{
+  public static void main(String args[])
+  {
+    int a=10;
+    int b=20;
+    int c=a+b ; 
+    int d=a-b ; 
+   long e=a*b ;  
+   long f=a/b ; 
+   long g=a%b ;  
+   System.out.println("Addition Value: "+c +"\nSubtraction: "+d+"\nMultiplication: "+e+"\nDivision(Quotient): "+f+"\nDivison(Remainder): "+g);
+    
+  }
+}
