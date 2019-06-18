3f05278315ee2cbffc2cf83fa7c415fa82d4f1b4
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-06-18 21:46:01 +0530

    Create condition_if.java

---
+++ condition_if.java
@@ -0,0 +1,15 @@
+// IF CONDITION ELIGIBILITY FOR VOTE
+class condition_if
+{
+  public static vooid main(String args[])
+  {
+      int n = 10;
+      if(n>=18)
+        {
+          System.out.println("You are Eligible");
+        }
+          System.out.println("You are not Eligible");
+    }
+}
+
+// output :- You are not Eligible.
