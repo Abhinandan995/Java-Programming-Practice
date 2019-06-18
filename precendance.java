ec11c26d9333b6acf28dbd558fcb24ba70dc2daa
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-06-18 21:39:14 +0530

    Create precendance.java

---
+++ precendance.java
@@ -0,0 +1,16 @@
+class precedance
+{
+  public static void main(String args[])
+  {
+    System.out.println(5*10/2+5-9);
+    /* step 1 : Multiplication
+	 * Step 2 : division
+	 * Step 3 : addition
+	 * Step 4 : subtraction
+   * if any paranthesis are available they will be first in order.
+	 * /
+   
+   }
+  }
+  
+  // output will be 21.
