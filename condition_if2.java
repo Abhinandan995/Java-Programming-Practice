b8faa143fed68cf5300d857edcc10724e828665c
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-06-18 21:49:45 +0530

    Create condition_if2.java

---
+++ condition_if2.java
@@ -0,0 +1,19 @@
+// If based another program to verify what will done when we given right input
+class condition_if2
+{
+  public static void main(String args[])
+  {
+    int n = 20;
+    if (n>=18)
+    {
+      System.out.println("You are Eligible");
+    }
+      System.out.println("YOu are Not Eligible");
+   }
+   
+}
+
+
+// Error here is if the condition is true it will execute the both the statements.
+// to over come an error here we use IF - ELSE statement
+     
