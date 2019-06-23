727c183550bca361c8a2ee7cf42753e8ce5be315
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-06-23 18:06:07 +0530

    Create ifcondition.java

---
+++ ifcondition.java
@@ -0,0 +1,19 @@
+class ifcondition {
+	public static void main(String args[]) {
+		int a = 20;
+		int b = 30;
+		int c = 30;
+		int d = 10;
+		if((a+b)>(c+d))
+		{
+			System.out.print("The sum of A+B: "+(a+b));
+			System.out.println("\tGreater");
+		}
+		else
+		{
+			System.out.print("The sum of C+D: "+(c+d));
+			System.out.println("Greater");
+		}
+	}
+
+}
