0f449337136cc65e429175a89c33a23ce9d052eb
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-06-24 20:59:02 +0530

    Create forloop.java

---
+++ forloop.java
@@ -0,0 +1,11 @@
+// printing of numbers from 1 to 10 using for loop
+class Forloop {
+	public static void main(String args[])
+	{
+		for(int i = 1;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
