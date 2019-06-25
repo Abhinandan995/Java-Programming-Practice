7c3a7e3ffa5aac877bfc5a3a25cbb0137f494d83
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-06-25 19:23:25 +0530

    Create Methodnew1.java

---
+++ Methodnew1.java
@@ -0,0 +1,13 @@
+// Method to topic which I have Covered
+class Methodnew1
+{
+	void status()
+	{
+		System.out.println("Completed \n For Loop \n If Condition  \n Variables");
+	}
+	public static void main(String args[])
+	{
+		Methodnew1 m1 = new Methodnew1();
+		m1.status();
+	}
+}
