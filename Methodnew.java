7c9337812be6354a2663f3a6ee5908c2dddacfd5
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-06-25 19:11:52 +0530

    Create Methodnew.java

---
+++ Methodnew.java
@@ -0,0 +1,13 @@
+// Print Hello Statement Using Method
+class Methodnew
+{
+	void Hello()
+	{
+		System.out.println("Hello World");
+	}
+	public static void main(String args[])
+	{
+		Methodnew m1 = new Methodnew();
+		m1.Hello();
+	}
+}
