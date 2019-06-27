ab4a8ca37f122add4b7803b399885b2ce6ef8614
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-06-27 21:10:12 +0530

    Create methodsum.java

---
+++ methodsum.java
@@ -0,0 +1,20 @@
+// sum of the numbers using method
+class testing {
+	int print() {
+		int a = 30;
+		int b = 40;
+		int c = a + b;
+		System.out.println(c);
+		return a + b;
+	}
+}
+
+public class methodtoprint {
+
+	public static void main(String[] args) {
+		testing t1 = new testing();
+		t1.print();
+
+	}
+
+}
