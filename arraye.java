2f1f7b36042a9747fa55bd52f9f4080be293c689
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-07-04 10:34:09 +0530

    Create arraye.java

---
+++ arraye.java
@@ -0,0 +1,10 @@
+public class Arraye {
+	public static void main(String args[]) {
+		// String week[] = new String[];
+		int i;
+		String[] week = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+		for (i =0; i <=week.length - 1; i++) {
+			System.out.println(week[i]);
+		}
+	}
+}
