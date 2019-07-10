305cd4bdaa933fa174a4ab63f9c2c651be7843e7
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-07-10 21:27:59 +0530

    Create FunctionRunner.java

---
+++ FunctionRunner.java
@@ -0,0 +1,14 @@
+import java.util.List;
+class FunctionRunner
+{
+  public static void main(String args[])
+  {
+    List <Integers> numbers = List.of(1,2,3,4,5,6,7,8,9,10);
+    checking(numbers);
+   }
+   public static void checking(List <Integer> numbers)
+   {
+    numbers.Stream.filter(elements -> elements%2 == 1).foreach(elements -> System.out.println(elements));\\ for odd numbers
+    \\ for even the syntax will be: -  element -> elements%2 == 0.
+   }
+  }
