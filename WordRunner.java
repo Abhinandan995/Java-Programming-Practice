08f0688cf49ca253893670ac3251184cef41b6bb
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-07-10 21:21:43 +0530

    Create WordRunner.java

---
+++ WordRunner.java
@@ -0,0 +1,13 @@
+import java.util.List;
+class WordRunner
+{
+  public static void main(String args[])
+  {
+    List < String > word = List.of("Apple","Bat","Cat","Dog");
+    functionfilter(words);
+   }
+   public static void functionprocess(List <String> word)
+   {
+     word.Stream().filter(element -> element.endwith("at").foreach(element -> System.out.println(element));
+    }
+   }
