313a956ad60921a15c86ebea41364cc82d96c13e
Abhinandan Reddy Pulagam <abhinandan995@gmail.com>
2019-07-10 21:22:28 +0530

    Update NumberRunner.java

--- NumberRunner.java
+++ NumberRunner.java
@@ -4,9 +4,9 @@ class NumberRunner
   public static void main(String args[])
   {
     List < Integer > number = List.of(1,2,3,4,5,6,7);
-    functionprocess();
+    functionprocess(number);
    }
-   public static void functionprocess()
+   public static void functionprocess(List <Integer> number)
    {
      number.Stream().foreach(element -> System.out.println(element));
     }
